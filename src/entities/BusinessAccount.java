4c6ea3d44ead333bc0e6a84eef93fe9ea49fda54
Vinícius Dabian <vinicius_dabian@hotmail.com>
2019-11-09 20:57:33 -0300

    Alteration in Program class

---
+++ src/entities/BusinessAccount.java
@@ -0,0 +1,38 @@
+package entities;
+
+public class BusinessAccount extends Account{
+
+private Double loanlimit;
+	
+	public BusinessAccount () {
+		super();
+	}
+	
+	public BusinessAccount (Integer number, String holder, Double balance, Double loanlimit) {
+		super(number, holder, balance);
+		this.loanlimit = loanlimit;
+	}
+	
+	public Double getLoanlimit() {
+		return loanlimit;
+	}
+	
+	public void setLoanLimit (Double loanlimit) {
+		this.loanlimit = loanlimit;
+	}
+	
+	public void loan(double amount) {
+		if (amount <= loanlimit) {
+			balance  += amount - 10.0;
+		}
+		
+	}
+	
+	@Override
+	public void withdraw(double amount) {
+		super.withdraw(amount);
+		balance -= 2.0;
+		
+	}
+	
+}
